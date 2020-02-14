5ae0a99e8599ff2ae52bd034e79467eeb877848d
Aaron Bradfield <abradfie@ashland.edu>
2020-02-13 19:26:26 -0500

    small fixes, styling fixes for landing page, updated schema, fixed init to use auth

--- Pycket/pycket/__init__.py
+++ Pycket/pycket/__init__.py
@@ -22,12 +22,15 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    
-    @app.route('/')
-    def index():
-        return render_template('index.html')
+
+    from . import db
+    db.init_app(app)
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
     return app
--- Pycket/pycket/auth.py
+++ Pycket/pycket/auth.py
@@ -15,6 +15,7 @@ def register():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
+        email = request.form["email"]
         db = get_db()
         error = None
 
@@ -22,6 +23,8 @@ def register():
             error = "Username is required."
         elif not password:
             error = "Password is required."
+        elif not email:
+            error = "An email is required."
         elif (
             db.execute("SELECT id FROM user WHERE username = ?", (username,)).fetchone()
             is not None
@@ -30,7 +33,7 @@ def register():
 
         if error is None:
             db.execute(
-                "INSERT INTO user (username, password) VALUES (?, ?)",
+                "INSERT INTO user (username, password, email) VALUES (?, ?, ?)",
                 (username, generate_password_hash(password)),
             )
             db.commit()
